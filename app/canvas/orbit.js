3965a40cc6af6448fb60ca7fe45d6a6f262b8d12
Keisuke Kido <kskkido@KeisukenoMacBook-puro.local>
2019-09-13 16:46:16 +0900

    apply trig to x-axis

---
+++ app/canvas/orbit.js
@@ -0,0 +1,43 @@
+import raf from 'utils/raf';
+
+export const draw = (
+  context,
+  {
+    width,
+    height
+  }
+) => {
+  const setupBounce = scope => (
+    Object.assign(
+      () => {
+        context.clearRect(0, 0, width, height);
+        context.beginPath();
+        context.arc(scope.centerX + Math.cos(scope.angle) * scope.offset, scope.centerY + Math.sin(scope.angle) * scope.offset, 50, 0, Math.PI * 2, false);
+        context.fill();
+      },
+      {
+        next: () => (
+          setupBounce({
+            ...scope,
+            angle: scope.angle + scope.speed
+          })
+        )
+      }
+    )
+  )
+
+  const scopedRender = animate => (
+    (p) => {
+      animate(p);
+      raf(scopedRender(animate.next()));
+    }
+  );
+
+  scopedRender(setupBounce({
+    centerY: height * .5,
+    centerX: width * .5,
+    offset: height * 0.3,
+    speed: 0.05,
+    angle: 0,
+  }))()
+};
--- app/components/App/index.js
+++ app/components/App/index.js
@@ -1,9 +1,13 @@
 import { tap, pipe } from 'ramda';
 import * as scatteredLines from 'canvas/scatteredLines';
-import * as bounce from 'canvas/bounce';
 import * as sineWave from 'canvas/sineWave';
 import * as cosineWave from 'canvas/cosineWave';
 import * as tangentWave from 'canvas/tangentWave';
+import * as bounce from 'canvas/bounce';
+import * as orbit from 'canvas/orbit';
+import * as cross from 'canvas/cross';
+import * as sineWaveBounce from 'canvas/sineWaveBounce';
+import * as orbitTriad from 'canvas/orbitTriad';
 import Canvas from '../Canvas';
 
 const App = () =>
@@ -14,6 +18,10 @@ const App = () =>
       tap(el => el.appendChild(Canvas({ draw: cosineWave.draw }))),
       tap(el => el.appendChild(Canvas({ draw: tangentWave.draw }))),
       tap(el => el.appendChild(Canvas({ draw: bounce.draw }))),
+      tap(el => el.appendChild(Canvas({ draw: orbit.draw }))),
+      tap(el => el.appendChild(Canvas({ draw: cross.draw }))),
+      tap(el => el.appendChild(Canvas({ draw: sineWaveBounce.draw }))),
+      tap(el => el.appendChild(Canvas({ draw: orbitTriad.draw })))
     ),
     document.createElement('p')
   );
